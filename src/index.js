75b88eaf0d0aaf6ffb620b49bdbbf17a8e74eb13
frustrated-coder <d.n_mwangi@outlook.com>
2022-09-01 14:58:56 +0300

    added eventListener for 'TimeIsUp'

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { init, onKeyUp,onSecondElapsed } from './eventHandlers.js'
+import { init, onKeyUp,onSecondElapsed,onTimeIsUp } from './eventHandlers.js'
 
 const textArea = document.querySelector('#type-area')
 const timerElement = document.querySelector('#timer')
@@ -8,6 +8,7 @@ const timerElement = document.querySelector('#timer')
 document.addEventListener('DOMContentLoaded',init)
 textArea.addEventListener('keyup',onKeyUp)
 timerElement.addEventListener('second',onSecondElapsed)
+timerElement.addEventListener('TimeIsUp',onTimeIsUp.bind(textArea))
 
 
 
