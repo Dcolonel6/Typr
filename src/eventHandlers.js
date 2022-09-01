4de115037d3197c76e37bc56602bd865bd4acbee
frustrated-coder <d.n_mwangi@outlook.com>
2022-09-01 15:34:11 +0300

    refactored onTimeIsUp

--- src/eventHandlers.js
+++ src/eventHandlers.js
@@ -7,8 +7,12 @@ import { Timer } from './timer.js'
 import { findWordRecords } from './utilities.js'
 
 const app = new App()
-const timer = new Timer(15)
 const paragraphEle = document.querySelector('#sentences')
+const timeElement = document.querySelector('#timer')
+const timer = new Timer(15,timeElement)
+console.log(timer, timeElement)
+
+
 
 
 //handler for DomContentLoaded
@@ -30,33 +34,41 @@ function onKeyUp(event){
     //check if we have collected any data for this typing session.If we havent start count down
     const sentenceToBeTyped = app.sentenceToBeTyped
     const arrayOfWords = sentenceToBeTyped.split(' ').slice(0,sentenceTyped.length)
-    arrayOfWords.forEach((word,index) => {
-        const wordTyped = listOfWordsTyped[index]
-        const wordData = findWordRecords(app.preparedSentenceToBeTyped,{'indexOfWord':index}) 
+    // arrayOfWords.forEach((word,index) => {
+    //     const wordTyped = listOfWordsTyped[index]
+    //     const wordData = findWordRecords(app.preparedSentenceToBeTyped,{'indexOfWord':index}) 
                
-        wordData.forEach((letterObject,indx) => {
+    //     wordData.forEach((letterObject,indx) => {
                       
-            const span = paragraphEle.querySelector(`span[data-indexInRelationToSentence="${letterObject['data-indexInRelationToSentence']}"]`)
+    //         const span = paragraphEle.querySelector(`span[data-indexInRelationToSentence="${letterObject['data-indexInRelationToSentence']}"]`)
             
-            if(letterObject["data-letter"] === wordTyped[indx]){debugger
+    //         if(letterObject["data-letter"] === wordTyped[indx]){
                 
-                span.classList.remove('text-danger')
-                span.classList.add('text-success')
+    //             span.classList.remove('text-danger')
+    //             span.classList.add('text-success')
 
-            }else{
-                span.classList.remove('text-success')
-                span.classList.add('text-danger')
+    //         }else{
+    //             span.classList.remove('text-success')
+    //             span.classList.add('text-danger')
 
-            }
-        })
-    })
+    //         }
+    //     })
+    // })
 
 }
 
 function onSecondElapsed(event){
     const { target,detail } = event
-    target.textContent = detail.timer.timeAlloted
+    target.querySelector('h2').textContent = detail.time
     
 }
+function onTimeIsUp(event){
+    //over ridden this to point to the text area element
+    const { target, detail} = event
+    this.setAttribute('disabled',true)
+    target.querySelector('h2').textContent = detail.timer.timeAlloted
+    
 
-export { init, onKeyUp }
+}
+
+export { init, onKeyUp,onSecondElapsed,onTimeIsUp }
