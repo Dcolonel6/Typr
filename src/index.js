b53e67dabc5d9847eebd94d588d3845dd4696402
frustrated-coder <d.n_mwangi@outlook.com>
2022-09-01 14:01:40 +0300

    added an event listener for the 'second' event

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
+import { init, onKeyUp,onSecondElapsed } from './eventHandlers.js'
+
+const textArea = document.querySelector('#type-area')
+const timerElement = document.querySelector('#timer')
+
+
 
 document.addEventListener('DOMContentLoaded',init)
+textArea.addEventListener('keyup',onKeyUp)
+timerElement.addEventListener('second',onSecondElapsed)
 
 
 
