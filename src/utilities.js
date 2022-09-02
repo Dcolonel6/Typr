81646cea1da533fc10e100a39e55f56e9f5ba77b
frustrated-coder <d.n_mwangi@outlook.com>
2022-09-02 17:28:22 +0300

    refactored the findWordRecords, its run time has been improved from O(n) to O(nlogn) or O(log n)

--- src/utilities.js
+++ src/utilities.js
@@ -1,5 +1,5 @@
 //this module will contain helper functions
-import { _ } from 'underscore'
+// import { _ } from 'underscore'
 
 
 function createElement(tagName, textContent, attributes = {}) {
@@ -8,7 +8,7 @@ function createElement(tagName, textContent, attributes = {}) {
       return updateElement(element, textContent, attributes);
     }
     return element;
-  }
+}
 
 function updateElement(element, text = "", attribute = {}) {
     if (text.length) {
@@ -20,9 +20,52 @@ function updateElement(element, text = "", attribute = {}) {
     return element;
 }
 
-function findWordRecords(list,attrib){
-  return _.where(list,attrib)
-
+function findWordRecords(list, queryObject){
+  const findLetterObj = findByPosition(list,queryObject)
+  return findTheWholeWordCollection(list, findLetterObj)
+  
+  
 }
+//finds using binary search
+function findByPosition(list, queryObject){
+  const wordIndex =  queryObject['indexOfWord']
+  const wordToSearch = queryObject['word'] 
+  let lastIndex = list.length -1
+  let firstIndex = 0  
+  let count = 0
+  while(firstIndex < lastIndex){   
+    
+    let mid = Math.floor((firstIndex + lastIndex) / 2)
+    let middleIndexWord = list[mid]['data-indexOfWord']
+    if(middleIndexWord < wordIndex){
+      if(mid == list.length - 1){
+        break;
+      }
+      firstIndex = mid + 1
+    }else if(list[mid]['data-indexOfWord'] > wordIndex){
+      lastIndex = mid - 1
+    }
+    else{
+      
+      return list[mid]
+    }   
+
+  }  
+  return {}
+}
+//slice the data using our find element and returns the whole word
+function findTheWholeWordCollection(list, letterObject){
+  if(Object.keys(letterObject).length > 0){
+    const indexOfLetterInWord = letterObject['data-letterIndxInWord']
+    const indexOfLetterInSentence = letterObject["data-indexInRelationToSentence"]
+    const lengthOfWord = letterObject["data-word"].length
+    const start = indexOfLetterInSentence - indexOfLetterInWord
+    const end = start + lengthOfWord
+    return list.slice(start,end)
+  }
+  return []
+  
+}
+
 
 export { createElement, findWordRecords}
